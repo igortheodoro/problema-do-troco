41a97a8190fa7ba04012830e3a8886687b0dec76
Igor <igortheodoro12@gmail.com>
2021-02-28 20:17:57 -0300

    First commit

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ProblemaDoTroco
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var moedasTroco = Troco(23);
+
+            foreach (var moeda in moedasTroco)
+            {
+                Console.Write($" {moeda}");
+            }
+        }
+
+        static List<int> Troco(int valor)
+        {
+            var moedas = new int[] { 100, 25, 10, 5, 1 };
+            var troco = new List<int>();
+
+            for (int i = 0; i < moedas.Length; i++)
+            {
+                var divisao = valor / moedas[i];
+
+                if (divisao > 0)
+                {
+                    var moedasDiv = divisao * moedas[i];
+                    valor -= moedasDiv;
+
+                    for (int j = 0; j < divisao; j++)
+                    {
+                        troco.Add(moedas[i]);
+                    }
+                }
+            }
+
+            return troco;
+        }
+    }
+}
